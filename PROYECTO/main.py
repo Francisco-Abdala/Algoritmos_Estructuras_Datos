26dea9912513018ee8a337ad3d361984ffaaa902
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-11-30 18:25:07 -0300

    Update main.py

--- PROYECTO/main.py
+++ PROYECTO/main.py
@@ -2,22 +2,27 @@ from secuencias import *
 import csv
 
 class Seq():
-    def __init__(self,dato):
+    def __init__(self,dato,valor):
         self.secuencia = dato
-        self.calidad = 100.00
+        self.repeticion = valor
+        self.calidad = 0
 
-    def restar_calidad(self):
-        self.calidad = 100.00 - 0.01
+    def restar_calidad(self,valor):
+        self.calidad = 100.00 - valor*0.01
 
 crear_csv()
 objetos = []
-
+secu = []
 with open("secuencias.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         seq = Seq(row)
         objetos.append(seq)
+        secu.append(row)
 
-print(len(objetos))
+print(secu)
 
+#ORDENAR
+#BUSCAR
+#LLAMAR FUNCIÓN DE RESTAR CALIDAD
 
